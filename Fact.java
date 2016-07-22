ce1d4d860d4c047e0d3f5155506d90cbad48abb5
Rajeshsrk16 <rajeshkumar.cse@ksrce.ac.in>
2016-07-22 15:27:03 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -2,8 +2,8 @@
     class Fact{  
      public static void main(String args[]){  
       int i,fact=1;  
-      int number=5;//It is the number to calculate factorial    
-      for(i=1;i<=number;i++){    
+      int num=5;   
+      for(i=1;i<=num;i++){    
           fact=fact*i;    
       }    
       System.out.println("Factorial of "+number+" is: "+fact);    
